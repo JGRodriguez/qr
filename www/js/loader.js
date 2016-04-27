864af964f2c82e2ed9e637bbf5ef7a481b4a16a7
CamiloAPR <palaciocamilor@gmail.com>
2016-04-27 01:39:39 -0500

    1erAvance RouteSchedule
    Actualización de los datos de cada RouteSchedule cada 2 segundos,
    (Incluye cambi ode la imagen de los buses)

--- www/js/loader.js
+++ www/js/loader.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
             lista.listview('refresh'); 
             // $('#listaConductores').listview('refresh');
         }).fail( function(d, textStatus, error) {
-            console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+            console.log("getJSON failed, status: " + textStatus + ", error: "+error)
         });   
 
    }else if(window.location.href.indexOf('#buses')>-1){
@@ -67,66 +67,49 @@ $(document).ready(function(){
             lista.listview('refresh'); 
             // $('#listaConductores').listview('refresh');
         }).fail( function(d, textStatus, error) {
-            console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+            console.log("getJSON failed, status: " + textStatus + ", error: "+error)
         });   
 
    }else if(window.location.href.indexOf('#rutas')>-1){
-        alert("Rutas");
-        lista=$('#listaRutas');
-        itemHTML="<li>"+
-                        "<div class='ui-grid-b'>"+
-                                "<div class='ui-block-b' style='width: 65%;'>"+
-                                    "<div data-role='fieldcontain' class='ui-field-contain'>"+
-                                            "<p><h2>nombreRuta</h2></p>"+
-                                            "<p><b>Origen: origenRuta</b></p>"+
-                                            "<p><b>Destino: destinoRuta</b></p>"+
+        $.fn.actualizarRutas = function(){
+                lista=$('#listaRutas');
+                itemHTML="<li>"+
+                                "<div class='ui-grid-b'>"+
+                                        "<div class='ui-block-b' style='width: 65%;'>"+
+                                            "<div class='ui-block-a' style='width: 20%;'>"+
+                                                "<img class='imgList' src='availability' height='50' width='50'>"+
+                                            "</div>"+
+                                            "<div data-role='fieldcontain' class='ui-field-contain'>"+
+                                                    "<p><h2>nombreRuta</h2></p>"+
+                                                    "<p><b>Origen: origenRuta</b></p>"+
+                                                    "<p><b>Destino: destinoRuta</b></p>"+
+                                                    "<p><b>Puestos disponibles: puestosDisponibles</b></p>"+
+                                            "</div>"+
+                                        "</div>"+
+                                        "<div class='ui-block-c' style='width: 6%;  float: right;'>"+
+                                            "<div style='float: right;'>"+
+                                               "<a href='#' data-role='button' data-icon='gear' data-iconpos='notext' data-theme='c' data-inline='true' class='ui-link ui-btn ui-btn-c ui-icon-flag ui-btn-icon-notext ui-btn-inline ui-shadow ui-corner-all' role='button'>Ver</a><br>"+
+                                            "</div>"+
+                                        "</div>"+    
                                     "</div>"+
-                                "</div>"+
-                                "<div class='ui-block-c' style='width: 6%;  float: right;'>"+
-                                    "<div style='float: right;'>"+
-                                       "<a href='#' data-role='button' data-icon='gear' data-iconpos='notext' data-theme='c' data-inline='true' class='ui-link ui-btn ui-btn-c ui-icon-gear ui-btn-icon-notext ui-btn-inline ui-shadow ui-corner-all' role='button'>Configurar</a><br>"+
-                                       "<a href='#' data-role='button' data-icon='forbidden' data-iconpos='notext' data-theme='c' data-inline='true' class='ui-link ui-btn ui-btn-c ui-icon-forbidden ui-btn-icon-notext ui-btn-inline ui-shadow ui-corner-all' role='button'>Eliminar</a>"+
-                                    "</div>"+
-                                "</div>"+    
-                            "</div>"+
-                      "</li>";
-        $.getJSON("testR.json", function( data ) {
-            console.log(data.length);
-            for(var i=0;i<data.length;i++){
-                lista.append(itemHTML.replace('nombreRuta',data[i].nameRS).replace('origenRuta',+data[i].sourceRS).replace('destinoRuta',data[i].destinationRS));
-            }
-            lista.listview('refresh'); 
-        }).fail( function(d, textStatus, error) {
-            console.error("getJSON failed, status: " + textStatus + ", error: "+error)
-        });   
-
+                              "</li>";
+                $.getJSON(serverURL+"/route_schedule", function( data ) {
+                    console.log(data.length);
+                    for(var i=0;i<data.length;i++){
+                        if(data[i].availability<0.5){
+                            itemHTML.replace('availability','/img/bus_dispo.png')
+                        }else if(data[i].availability<0.5){
+                            itemHTML.replace('availability','/img/bus_mediolleno.png')
+                        }else{
+                            itemHTML.replace('availability','/img/bus_lleno.png')
+                        }
+                        lista.append(itemHTML.replace('nombreRuta',data[i].nameRS).replace('origenRuta',data[i].sourceRS).replace('destinoRuta',data[i].destinationRS).replace('puestosDisponibles',data[i].availability*data[i].bus.capacity));
+                    }
+                    lista.listview('refresh'); 
+                }).fail( function(d, textStatus, error) {
+                    console.log("getJSON failed, status: " + textStatus + ", error: "+error)
+                }); 
+        }  
+        setInterval(actualizarRutas, 2000);
    }
-    // else if(window.location.href.indexOf('#buses')>-1){
-    //     lista= document.getElementById('listaBuses');
-    //     $.getJSON( 'js/test.json', function( data ) {
-    //         // alert(" "+data.length);
-    //         itemHTML="<li>"+
-    //                         "<div class='ui-grid-b'>"+
-    //                             "<div class='ui-block-a' style='width: 20%;'>"+
-    //                                 "<img class='imgList' src='linkImagen' height='50' width'50'=''>"+
-    //                             "</div>"+
-    //                             "<div class='ui-block-b' style='width: 65%;'>"+
-    //                                 "<div data-role='fieldcontain' class='ui-field-contain'>"+
-    //                                         "<h2>placaBus</h2>"+
-    //                                         "<p><b>capacidadBus</b></p>"+
-    //                                 "</div>"+
-    //                             "</div>"+
-    //                             "<div class='ui-block-c' style='width: 6%;  float: right;'>"+
-    //                                 "<div style='float: right;'>"+
-    //                                    "<a href='#' data-role='button' data-icon='gear' data-iconpos='notext' data-theme='c' data-inline='true' class='ui-link ui-btn ui-btn-c ui-icon-gear ui-btn-icon-notext ui-btn-inline ui-shadow ui-corner-all' role='button'>Configurar</a><br>"+
-    //                                    "<a href='#' data-role='button' data-icon='forbidden' data-iconpos='notext' data-theme='c' data-inline='true' class='ui-link ui-btn ui-btn-c ui-icon-forbidden ui-btn-icon-notext ui-btn-inline ui-shadow ui-corner-all' role='button'>Eliminar</a>"+
-    //                                 "</div>"+
-    //                             "</div>"+    
-    //                         "</div>"+
-    //                       "</li>";
-    //         for(var i=0;i<items.length;i++){
-    //             totalItemsHTML=totalItemsHTML+itemHTML.replace('linKImagen','data[i].img').replace('placaBus',data[i].plate).replace('capacidadBus',data[i].capacity);
-    //         }   
-    //         lista.innerHTML=totalItemsHTML;
-    // }
 });
